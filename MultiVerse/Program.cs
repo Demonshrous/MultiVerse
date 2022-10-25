6870917c7e83345e0a6aa6beb2e9272dd67550fe
Diboune Clement <clement.diboune99@gmail.com>
2022-10-25 11:45:38 +0200

    Changement print local

--- MultiVerse/Program.cs
+++ MultiVerse/Program.cs
@@ -7,7 +7,7 @@ namespace MultiVerse
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Hello World Test!");
+            Console.WriteLine("Hello World Test changement local!");
 
         }
     }
