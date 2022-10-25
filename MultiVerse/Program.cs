f087afbda5328bdd49db92065975a9b342bc8a41
Diboune Clement <clement.diboune99@gmail.com>
2022-10-25 12:05:53 +0200

    Changement avant merge

--- MultiVerse/Program.cs
+++ MultiVerse/Program.cs
@@ -6,7 +6,7 @@ namespace MultiVerse
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World C!");
+            Console.WriteLine("Hello World C test 2!");
         }
     }
 }
